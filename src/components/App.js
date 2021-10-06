8678410785849caced2549b9a42b9e6f6685ddd3
Ryo1026 <c9pcv5nvbp@gmail.com>
2021-10-06 18:22:47 +0800

    10/6 Redux初學

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../App.css";
+import Footer from "./Footer";
+import AddTodo from "../containers/AddTodo";
+import VisibleTodoList from "../containers/VisibleTodoList";
+
+function App() {
+  return (
+    <div>
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { addTodo } from "../actions/actions";
+
+let AddTodo = ({ dispatch }) => {
+  let input;
+  return (
+    <div>
+      <from
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = "";
+        }}
+      >
+        <input
+          ref={(node) => {
+            input = node;
+          }}
+        />
+        <button type="submit">新增Todo</button>
+      </from>
+    </div>
+  );
+};
+
+AddTodo = connect()(AddTodo);
+
+export default AddTodo;
